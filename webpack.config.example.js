023db66933267684854c0ba455169f5a1adc5360
Мансуров Дмитрий <dmansurov83@gmail.com>
2018-02-10 16:11:41 +0300

    enable example sourcemaps

--- webpack.config.example.js
+++ webpack.config.example.js
@@ -8,6 +8,7 @@ module.exports = {
         path: path.resolve(__dirname, 'build-example'),
         filename: 'index.js',
     },
+    devtool: 'source-map',
     module: {
         rules: [
             {
