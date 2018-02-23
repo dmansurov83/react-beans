17caa8397838ebd1c330e3bcde81b9ac422c9485
Мансуров Дмитрий <dmansurov83@gmail.com>
2018-02-23 15:59:16 +0300

    fix publishing

--- webpack.config.js
+++ webpack.config.js
@@ -1,32 +1,27 @@
 var path = require('path');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-    entry: './src/index.js',
+    target: 'web',
+    entry: './example/src/index.js',
     output: {
         path: path.resolve(__dirname, 'build'),
         filename: 'index.js',
-        libraryTarget: 'commonjs2' // THIS IS THE MOST IMPORTANT LINE! :mindblow: I wasted more than 2 days until realize this was the line most important in all this guide.
     },
+    devtool: 'source-map',
     module: {
         rules: [
             {
                 test: /\.js$/,
-                include: path.resolve(__dirname, 'src'),
+                include: [path.resolve(__dirname, 'example'), path.resolve(__dirname, 'src')],
                 exclude: /(node_modules|build)/,
                 use: {
                     loader: 'babel-loader',
-                    options: {
-                        presets: [
-                            "es2015",
-                            "stage-0",
-                            "react"
-                        ]
-                    }
                 }
             }
         ]
     },
-    externals: {
-        'react': 'commonjs react' // this line is just to use the React dependency of our parent-testing-project instead of using our own React.
-    }
+    plugins: [new HtmlWebpackPlugin({
+        template: 'example/public/index.html'
+    })]
 };
